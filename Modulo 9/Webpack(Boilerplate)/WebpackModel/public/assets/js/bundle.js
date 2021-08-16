d3e32dcb53745abdf83576afd6a49ee8c5e49e80
João Vieira <mutantinotrue@outlook.com>
2021-08-16 12:36:32 -0300

    2. Webpack(Boilerplate)

---
+++ Modulo 9/Webpack(Boilerplate)/WebpackModel/public/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//
+//
+//
+console.log('Linha 4');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Modulo 9/Webpack(Boilerplate)/WebpackModel/src/index.js
@@ -0,0 +1,4 @@
+//
+//
+//
+console.log('Linha 4')
