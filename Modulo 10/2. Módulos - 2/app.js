8bb5b2532cc5b9601d9f0b062e8d67278c994c5b
João Vieira <mutantinotrue@outlook.com>
2021-08-30 09:29:18 -0300

    Módulos 2

---
+++ Modulo 10/2. Módulos - 2/app.js
@@ -0,0 +1,16 @@
+/* const mult = require('./mod1')
+
+console.log(mult(5, 5)) */
+
+/* const Cachorro = require('./Z/mod2')
+
+const toto = new Cachorro('Totó')
+toto.latir() */
+
+/* 
+const path = require('path')
+console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'))
+ */
+
+/* console.log(__filename ) */
+/* console.log(__dirname) */
