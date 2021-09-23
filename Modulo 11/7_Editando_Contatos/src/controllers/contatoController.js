fb0f7961575e61ee34c3f1cd0ea0010c23be6dbd
João Vieira <mutantinotrue@outlook.com>
2021-09-22 22:34:26 -0300

    7_Editando_Contatos

---
+++ Modulo 11/7_Editando_Contatos/src/controllers/contatoController.js
@@ -0,0 +1,56 @@
+const Contato = require('../models/ContatoModel')
+
+exports.index = (req, res) => {
+  res.render('contato', {
+    contato: {}
+  })
+}
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body)
+    await contato.register()
+  
+    if(contato.erros.length > 0) {
+      req.flash('erros', contato.erros)
+      req.session.save(() => res.redirect('back'))
+      return
+    }
+  
+    req.flash('success', 'Contato registrado com sucesso')
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`))
+    return
+  } catch (e) {
+    console.log(e)
+    return res.render('404')
+  }
+}
+
+exports.editIndex = async (req, res) => {
+  if(!req.params.id) return res.render('404')
+
+  const contato = await Contato.buscarPorId(req.params.id)
+  if(!contato)return res.render('404')
+
+  res.render('contato', {contato})
+}
+
+exports.edit = async function(req, res) {
+  try {
+    if(!req.params.id) return res.render('404')
+    const contato = new Contato(req.body)
+    await contato.edit(req.params.id)
+  
+    if(contato.erros.length > 0) {
+      req.flash('erros', contato.erros)
+      req.session.save(() => res.redirect('back'))
+      return
+    }
+  
+    req.flash('success', 'Contato editado com sucesso')
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`))
+    return
+    } catch (e) {
+    res.render('404')
+  }
+}
